f2dc91f401de90bc5201c21501ebfb60142149fe
Pedro <poterp3@gmail.com>
2025-09-29 23:08:28 -0300

    god

--- client/components/BackgroundMusic.tsx
+++ client/components/BackgroundMusic.tsx
@@ -7,7 +7,7 @@ const BackgroundMusic = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
 
   // Free wedding music URL (you can replace with your preferred track)
-  const musicUrl = "/public/SparksColdplay.mp3";
+  const musicUrl = "/SparksColdplay.mp3";
 
   useEffect(() => {
     const audio = audioRef.current;
